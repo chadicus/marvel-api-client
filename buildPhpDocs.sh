0be71a56c54260c9e3b9d065767a7c9665aed85b
chadicus <chadwickgray@gmail.com>
2014-02-03 13:34:30 -0500

    Update buildPhpDocs
    Put phpdocs in subfolder

--- buildPhpDocs.sh
+++ buildPhpDocs.sh
@@ -5,7 +5,7 @@ composer install
 
 [ -d tmp ] && rm -fr tmp
 git clone -b gh-pages git@github.com:${GITHUB_USER}/marvel-api-client tmp
-./vendor/bin/phpdoc.php --directory src --target tmp --template responsive-twig --sourcecode --defaultpackagename Chadicus --title "Marvel API PHP Client"
+./vendor/bin/phpdoc.php --directory src --target tmp/docs --template responsive-twig --sourcecode --defaultpackagename Chadicus --title "Marvel API PHP Client"
 cd tmp
 git add .
 git commit -m "Build phpdocs"
