d4305a0b83ffa35789074563d2e040cefbfa115d
chadicus <chadwickgray@gmail.com>
2015-07-31 08:21:37 -0400

    Change 'amount' to 'price'

--- tests/Entities/PriceTest.php
+++ tests/Entities/PriceTest.php
@@ -11,8 +11,8 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
     /**
      * Verify invalid constructor parameters cause exceptions.
      *
-     * @param mixed $type   The description of the amount.
-     * @param mixed $amount The amount of the price.
+     * @param mixed $type   The description of the price.
+     * @param mixed $price The price of the price.
      *
      * @test
      * @covers ::__construct
@@ -21,9 +21,9 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
      *
      * @return void
      */
-    public function constructWithInvalidParameters($type, $amount)
+    public function constructWithInvalidParameters($type, $price)
     {
-        new Price($type, $amount);
+        new Price($type, $price);
     }
 
     /**
@@ -34,32 +34,32 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
     public function constructorBadData()
     {
         return [
-            'type is empty' => ['', 'a amount'],
-            'type is whitespace' => ["\t \n", 'a amount'],
-            'type is not a string' => [true, 'a amount'],
-            'amount is not a float' => ['a type', false],
+            'type is empty' => ['', 'a price'],
+            'type is whitespace' => ["\t \n", 'a price'],
+            'type is not a string' => [true, 'a price'],
+            'price is not a float' => ['a type', false],
         ];
     }
 
     /**
      * Verify valid constructor parameters cause no exceptions.
      *
-     * @param mixed $type   The description of the amount.
-     * @param mixed $amount The amount of the price.
+     * @param mixed $type   The description of the price.
+     * @param mixed $price The price of the price.
      *
      * @test
      * @covers ::__construct
-     * @covers ::getAmount
+     * @covers ::getPrice
      * @covers ::getType
      * @dataProvider constructorGoodData
      *
      * @return void
      */
-    public function constructWithValidParameters($type, $amount)
+    public function constructWithValidParameters($type, $price)
     {
-        $price = new Price($type, $amount);
-        $this->assertSame($type, $price->getType());
-        $this->assertSame($amount, $price->getAmount());
+        $entity = new Price($type, $price);
+        $this->assertSame($type, $entity->getType());
+        $this->assertSame($price, $entity->getPrice());
     }
 
     /**
@@ -70,10 +70,10 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
     public function constructorGoodData()
     {
         return [
-            'type is null, amount is null' => [null, null],
-            'type is null, amount is float' => [null, 1.0],
-            'type is string, amount is null' => ['a type', null],
-            'type is string, amount is float' => ['a type', 1.0],
+            'type is null, price is null' => [null, null],
+            'type is null, price is float' => [null, 1.0],
+            'type is string, price is null' => ['a type', null],
+            'type is string, price is float' => ['a type', 1.0],
         ];
     }
 
@@ -87,9 +87,9 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
      */
     public function fromArray()
     {
-        $price = Price::fromArray(['type' => 'a type', 'amount' => 1.0]);
+        $price = Price::fromArray(['type' => 'a type', 'price' => 1.0]);
         $this->assertSame('a type', $price->getType());
-        $this->assertSame(1.0, $price->getAmount());
+        $this->assertSame(1.0, $price->getPrice());
     }
 
     /**
@@ -103,7 +103,7 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
      */
     public function fromArrayWithInvalidInput()
     {
-        Price::fromArray(['type' => 'a type', 'amount' => 'amount not a float']);
+        Price::fromArray(['type' => 'a type', 'price' => 'price not a float']);
     }
 
     /**
@@ -118,16 +118,16 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
     {
         $prices = Price::fromArrays(
             [
-                ['type' => 'a type', 'amount' => 1.0],
-                ['type' => 'another type', 'amount' => 2.0],
+                ['type' => 'a type', 'price' => 1.0],
+                ['type' => 'another type', 'price' => 2.0],
             ]
         );
 
         $this->assertSame(2, count($prices));
         $this->assertSame('a type', $prices[0]->getType());
-        $this->assertSame(1.0, $prices[0]->getAmount());
+        $this->assertSame(1.0, $prices[0]->getPrice());
         $this->assertSame('another type', $prices[1]->getType());
-        $this->assertSame(2.0, $prices[1]->getAmount());
+        $this->assertSame(2.0, $prices[1]->getPrice());
     }
 
     /**
@@ -143,8 +143,8 @@ final class PriceTest extends \PHPUnit_Framework_TestCase
     {
         Price::fromArrays(
             [
-                ['type' => 'a type', 'amount' => 1.0],
-                ['type' => '', 'amount' => 2.0],
+                ['type' => 'a type', 'price' => 1.0],
+                ['type' => '', 'price' => 2.0],
             ]
         );
     }
