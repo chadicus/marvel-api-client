a73b648b1b7c514688ed48344da8bc590aecd8c8
chadicus <chadwickgray@gmail.com>
2017-04-10 20:20:17 -0400

    Remove dependency on mongo

--- tests/Assets/ArrayCache.php
+++ tests/Assets/ArrayCache.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Chadicus\Marvel\Api\Cache;
+namespace Chadicus\Marvel\Api\Assets;
 
 use DominionEnterprises\Util\Arrays;
 use Psr\SimpleCache\CacheInterface;
@@ -8,7 +8,7 @@ use Psr\SimpleCache\CacheInterface;
 /**
  * A PSR-16 implementation which stores data in an array.
  */
-final class ArrayCache extends AbstractCache implements CacheInterface
+final class ArrayCache implements CacheInterface
 {
     /**
      * Array containing the cached data.
@@ -24,12 +24,9 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param mixed  $default Default value to return if the key does not exist.
      *
      * @return mixed The value of the item from the cache, or $default in case of cache miss.
-     *
-     * @throws InvalidArgumentException Thrown if the $key string is not a legal value.
      */
     public function get($key, $default = null)
     {
-        $this->verifyKey($key);
         $cache = Arrays::get($this->cache, $key);
         if ($cache === null) {
             return $default;
@@ -53,14 +50,9 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      *                                     for it or let the driver take care of that.
      *
      * @return bool True on success and false on failure.
-     *
-     * @throws InvalidArgumentException Thrown if the $key string is not a legal value.
      */
     public function set($key, $value, $ttl = null)
     {
-        $this->verifyKey($key);
-        $this->verifyValue($value);
-
         $this->cache[$key] = [
             'response' => $value,
             'expires' => $this->getExpires($ttl),
@@ -75,12 +67,9 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param string $key The unique cache key of the item to delete.
      *
      * @return bool True if the item was successfully removed. False if there was an error.
-     *
-     * @throws InvalidArgumentException Thrown if the $key string is not a legal value.
      */
     public function delete($key)
     {
-        $this->verifyKey($key);
         unset($this->cache[$key]);
         return true;
     }
@@ -103,8 +92,6 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param mixed    $default Default value to return for keys that do not exist.
      *
      * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
-     *
-     * @throws InvalidArgumentException Thrown if the $key string is not a legal value.
      */
     public function getMultiple($keys, $default = null)
     {
@@ -125,9 +112,6 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      *                                      for it or let the driver take care of that.
      *
      * @return bool True on success and false on failure.
-     *
-     * @throws InvalidArgumentException Thrown if $values is neither an array nor a Traversable,
-     *                                  or if any of the $values are not a legal value.
      */
     public function setMultiple($values, $ttl = null)
     {
@@ -144,9 +128,6 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param iterable $keys A list of string-based keys to be deleted.
      *
      * @return bool True if the items were successfully removed. False if there was an error.
-     *
-     * @throws InvalidArgumentException Thrown if $keys is neither an array nor a Traversable,
-     *                                  or if any of the $keys are not a legal value.
      */
     public function deleteMultiple($keys)
     {
@@ -168,12 +149,9 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param string $key The cache item key.
      *
      * @return bool
-     *
-     * @throws InvalidArgumentException Thrown if the $key string is not a legal value.
      */
     public function has($key)
     {
-        $this->verifyKey($key);
         return isset($this->cache[$key]);
     }
 
@@ -183,8 +161,6 @@ final class ArrayCache extends AbstractCache implements CacheInterface
      * @param mixed $ttl The time-to-live value to validate.
      *
      * @return integer
-     *
-     * @throws InvalidArgumentException Thrown if the $ttl is not null, an integer or \DateInterval.
      */
     private function getExpires($ttl)
     {
@@ -200,6 +176,6 @@ final class ArrayCache extends AbstractCache implements CacheInterface
             return time() + $ttl->s;
         }
 
-        throw new InvalidArgumentException('$ttl must be null, an integer or \DateInterval instance');
+        throw new \InvalidArgumentException('$ttl must be null, an integer or \DateInterval instance');
     }
 }
