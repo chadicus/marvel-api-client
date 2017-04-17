751afece9a0c9c07a3d1a3fd28a0c2163e59e710
chadicus <chadwickgray@gmail.com>
2017-04-16 21:00:30 -0400

    Add return types to DataWrapper and DataContainer

--- src/DataWrapperInterface.php
+++ src/DataWrapperInterface.php
@@ -12,47 +12,47 @@ interface DataWrapperInterface
      *
      * @return integer
      */
-    public function getCode();
+    public function getCode() : int;
 
     /**
      * Returns A string description of the call status.
      *
      * @return string
      */
-    public function getStatus();
+    public function getStatus() : string;
 
     /**
      * Returns the copyright notice for the returned result.
      *
      * @return string
      */
-    public function getCopyright();
+    public function getCopyright() : string;
 
     /**
      * Returns the attribution notice for this result
      *
      * @return string
      */
-    public function getAttributionText();
+    public function getAttributionText() : string;
 
     /**
      * Returns an HTML representation of the attribution notice for this result.
      *
      * @return string
      */
-    public function getAttributionHTML();
+    public function getAttributionHTML() : string;
 
     /**
      * Returns a digest value of the content returned by the call.
      *
      * @return string
      */
-    public function getEtag();
+    public function getEtag() : string;
 
     /**
      * Returns the results returned by the call.
      *
      * @return DataContainerInterface
      */
-    public function getData();
+    public function getData() : DataContainerInterface;
 }
